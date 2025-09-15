5dd7908dec75dbc2ea9bf6c3647ae310e7bfaa75
andreaeusebi <eusandre95@gmail.com>
2025-09-15 12:20:09 +0200

    Fixed issue in getCustomLogger() function signature. Added a test script.

--- py_utils/log.py
+++ py_utils/log.py
@@ -1,13 +1,24 @@
 import logging
-from typing import Type, Union
+from typing import Union
 
-def getCustomLogger(logger_name_   : str,
-                    node_name_     : str,
-                    log_handler_   : Type[Union[logging.StreamHandler, logging.FileHandler]],
-                    logging_level_ : int):
+def getCustomLogger(logger_name_  : str,
+                    node_name_    : str,
+                    log_handler_  : Union[logging.StreamHandler, logging.FileHandler],
+                    logging_level_: int) -> logging.Logger:
     """
     Create and return a properly formatted Logger object associated with the given log handler.
+
+    Args:
+        logger_name_ (str): Name of the logger.
+        node_name_ (str): Node/module name to include in log messages.
+        log_handler_ (Union[logging.StreamHandler, logging.FileHandler]): 
+            A logging handler instance (e.g., logging.StreamHandler() or logging.FileHandler(...)).
+        logging_level_ (int): Logging level (e.g., logging.INFO).
+
+    Returns:
+        logging.Logger: Configured logger instance.
     """
+
     logger = logging.getLogger(logger_name_)
     logger.setLevel(logging_level_)
 
@@ -21,3 +32,18 @@ def getCustomLogger(logger_name_   : str,
     log_handler_.setFormatter(log_formatter)
 
     return logger
+
+##### ----- Test Script ----- #####
+
+def test():
+    ## Test 1 - Check that the function works
+    logger = getCustomLogger(logger_name_=__name__,
+                             node_name_="First Node",
+                             log_handler_=logging.StreamHandler(),
+                             logging_level_=logging.INFO)
+    
+    logger.info("First call")
+    ## Test 1 - End
+
+if __name__ == "__main__":
+    test()
